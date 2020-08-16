ff8e93dd787fc21cb14ea8796738c4d62008f2ac
careyToboo <toboos@sina.com>
2020-08-16 13:53:58 +0800

    restart edtior

--- packages/craft/src/layout/code/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledCode = styled.div`
-  width: 230px;
-  height: 100%;
-  flex: 0 0 auto;
-  border-right: 1px solid rgb(219, 219, 219);
-  background-color: #fff;
-`
--- packages/craft/src/layout/reference/styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const StyledReference = styled.div`
-  width: 230px;
-  height: 100%;
-  flex: 0 0 auto;
-  border-left: 1px solid rgb(219, 219, 219);
-  background-color: #fff;
-`
